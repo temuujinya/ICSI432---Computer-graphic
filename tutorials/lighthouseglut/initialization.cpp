4328ef9bd77c4d08ab9a0fa16bb9336334185f12
USOTT <y.usott@gmail.com>
2018-11-16 20:08:42 +0800

    tuts initialization final comments

--- tutorials/lighthouseglut/initialization.cpp
+++ tutorials/lighthouseglut/initialization.cpp
@@ -7,13 +7,24 @@ void renderScene(void) {
     //clear previous  color
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); 
 
-	glBegin(GL_TRIANGLES);
-		glVertex3f(-0.5,-0.5,0.0);
+	glBegin(GL_LINES);
+        //triagle color
+        //just fill in the blanks with RGB values between 0 and 1
+		glColor3f(0.4,0.1,0.1);
+        glVertex3f(-0.5,-0.5,0.0);
+		glColor3f(0.4,0.3,0.9);
 		glVertex3f(0.5,0.0,0.0);
+		glColor3f(0.9,0.3,0.4);
 		glVertex3f(0.0,0.5,0.0);
 	glEnd();
 
+    // Using double buffering (GL_DOUBLE in the init function) OpenGL draws in the 
+    // back buffer and presents the contents from the front buffer in the window. 
+    // When swap buffers is called the buffers are switched, i.e. the previous back 
+    // buffer becomes the front buffer showing what was rendered, and the front buffer 
+    // becomes the back buffer where the next frame will be rendered.
 	glutSwapBuffers();
+
     //glFlush - use in case when GLUT_SINGLE is used in GLUT_DISPLAY_FUNC. Else if
 // GLUT_DOUBLE is used dont necessary to use it.
 }
@@ -35,3 +46,22 @@ int main(int argc, char **argv){
     glutMainLoop();
     return 1;
 }
+
+
+
+/*
+
+Q1: Can you explain the purpose of glutSwapBuffers() 
+and more about the Display Modes used in calling the glutInitDisplayMode() function.
+
+A1: Most graphic systems work with double or triple buffering. 
+What this means is that there is a buffer being presented on the 
+screen, while another is being drawn. At the end of the frame the 
+buffers are swapped. That’s why you set GLUT_DOUBLE on the display 
+settings. Another setting relates to the type of output, more 
+specifically we usually want an RGBA output, i.e. RGB plus alpha. 
+The final setting relates to the Z-buffer, aka depth buffer, which 
+stores the depths of the pixels currently on the screen, and prevents 
+a pixel from being written if its further away than the pixel in the 
+frame buffer.
+*/
