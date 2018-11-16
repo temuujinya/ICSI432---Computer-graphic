99c5c16c2c247ba805840041822762e9f889b39a
USOTT <y.usott@gmail.com>
2018-11-16 14:50:09 +0800

    tuts initialization add comments

--- tutorials/lighthouseglut/initialization.cpp
+++ tutorials/lighthouseglut/initialization.cpp
@@ -2,8 +2,10 @@
 
 
 void renderScene(void) {
-
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    //bg color
+    glClearColor(0.2,0.5,0.4,0.5);
+    //clear previous  color
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); 
 
 	glBegin(GL_TRIANGLES);
 		glVertex3f(-0.5,-0.5,0.0);
@@ -12,9 +14,15 @@ void renderScene(void) {
 	glEnd();
 
 	glutSwapBuffers();
+    //glFlush - use in case when GLUT_SINGLE is used in GLUT_DISPLAY_FUNC. Else if
+// GLUT_DOUBLE is used dont necessary to use it.
 }
 int main(int argc, char **argv){
     glutInit(&argc, argv);
+    
+    //Must include at least 1 argument
+    //that is type of buffer i-e SINGLE or DOUBLE buffer.here as I am dealing with the color so I
+    //have used GL_RGBA also as I have dealt with these 4 colours.
     glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
     glutInitWindowPosition(300,200);
     glutInitWindowSize(320,320);
